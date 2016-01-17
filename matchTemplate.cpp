c968d07c86b47dbbc860f13e3a4e67fa691bb91d
Aakash SONI <aakash_hellyeah@live.com>
2016-01-17 23:55:16 +0100

    Update matchTemplate.cpp

--- matchTemplate.cpp
+++ matchTemplate.cpp
@@ -7,42 +7,24 @@
 using namespace std;
 using namespace cv;
 
-// Input Variables.
-Mat img; Mat img2 ; Mat temp; Mat result; Mat result2;
-int found=0;
-// Output Windows
-String image_window = "Source Image";
-String result_window = "Result window";
-String templ_window = "Template window";
-
-// Variables
-int select_method;
-int trackbar_limit = 5;
-
-
-/** main */
+/* main */
 int main( int argc, char** argv )
 {
-  // Accept image and template
-  img = imread( argv[1], 1 ); // imread( image_file, flag )
-  temp = imread( argv[2], 1 ); // flag > 0 Returns a 3-channel color image.
+	Mat img;
+	int found = 0;
 
-  // Create windows
-  namedWindow( image_window, WINDOW_NORMAL  );
-  //resizeWindow(image_window, 800, 600);
-  namedWindow( result_window, WINDOW_NORMAL );
-  //resizeWindow(result_window, 800,600);
-  namedWindow( templ_window, WINDOW_AUTOSIZE   );
+  // Accept template
+	Mat temp = imread( argv[1], 1 );
 
-  // Create Trackbar
-  String trackbar_name = "Method:";
-  createTrackbar( trackbar_name, image_window, &select_method, trackbar_limit, matchingMethod );
-  //Accept image
- for(int i=2; i < argc; i++){
-  img = imread( argv[i], 1 ); // imread( image_file, flag )
+  // Accept images & then match.
+  for(int i=2; i < argc; i++){
+	  img = imread( argv[i], 1 ); // Accept Source Image.
+	  cout << "Comparing template with image " << i-1 << " : " << argv[i] << endl;
 
-  if(matchingMethod( 0, 0 ))
-	  break;
+  	  if(matchingMethod( img, temp )){ // Break loop if match found.
+  		 found=1; // Set found flag.
+  		 break;
+  	  }
   }
 
   if(!found){
